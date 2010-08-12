e92a348242032e39c3bcce82d8fc02cd318cf3db
Derek Kastner <dkastner@gmail.com>
2010-08-12 15:35:08 -0400

    Using emitter gem

--- lib/residence.rb
+++ lib/residence.rb
@@ -1,23 +1,10 @@
+require 'emitter'
+
 module BrighterPlanet
   module Residence
-    extend self
+    extend BrighterPlanet::Emitter
 
-    def included(base)
-      require 'cohort_scope'
-      require 'falls_back_on'
-      require 'falls_back_on/active_record_ext'
-
-      require 'residence/carbon_model'
-      require 'residence/characterization'
-      require 'residence/data'
-      require 'residence/summarization'
-
-      base.send :include, BrighterPlanet::Residence::CarbonModel
-      base.send :include, BrighterPlanet::Residence::Characterization
-      base.send :include, BrighterPlanet::Residence::Data
-      base.send :include, BrighterPlanet::Residence::Summarization
-    end
-    def residence_model
+    def self.residence_model
       if Object.const_defined? 'Residence'
         ::Residence
       elsif Object.const_defined? 'ResidenceRecord'
