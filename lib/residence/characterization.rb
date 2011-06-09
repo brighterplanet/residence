e8e5ddef76633583698fdf65fd0e3012058fd1f5
Seamus Abshere <seamus@abshere.net>
2011-06-09 01:19:36 -0500

    make tests green (except one about big_cohort) with new sniff

--- lib/residence/characterization.rb
+++ lib/residence/characterization.rb
@@ -14,31 +14,31 @@ module BrighterPlanet
           has :air_conditioner_use
           has :refrigerator_count
           has :freezer_count
-          has :lighting_efficiency, :measures => :percentage
-          has :floorspace_estimate, :trumps => :rooms, :measures => :area
+          has :lighting_efficiency#, :measures => :percentage
+          has :floorspace_estimate
           has :floors # not used
           has :bathrooms
           has :bedrooms
-          has :full_bathrooms, :hidden => true
-          has :half_bathrooms, :hidden => true
-          has :rooms, :trumps => [:bathrooms, :bedrooms, :full_bathrooms, :half_bathrooms]
+          has :full_bathrooms
+          has :half_bathrooms
+          has :rooms
           has :acquisition
           has :retirement
           has :construction_year # TODO show year only
-          has :occupation, :measures => :percentage
-          has :annual_fuel_oil_volume_estimate, :trumps => :annual_fuel_oil_cost, :measures => :volume
-          has :annual_fuel_oil_cost, :trumps => :annual_fuel_oil_volume_estimate, :measures => :cost
-          has :monthly_natural_gas_volume_estimate, :trumps => :monthly_natural_gas_cost, :measurement_options => { :external => :therms, :internal => :joules }
-          has :monthly_natural_gas_cost, :trumps => :monthly_natural_gas_volume_estimate, :measures => :cost
-          has :monthly_electricity_use_estimate, :trumps => :monthly_electricity_cost, :measures => :electrical_energy
-          has :monthly_electricity_cost, :trumps => :monthly_electricity_use_estimate, :measures => :cost
-          has :green_electricity, :measures => :percentage
-          has :annual_propane_volume_estimate, :trumps => :annual_propane_cost, :measures => :volume
-          has :annual_propane_cost, :trumps => :annual_propane_volume_estimate, :measures => :cost
+          has :occupation#, :measures => :percentage
+          has :annual_fuel_oil_volume_estimate, :measures => :volume
+          has :annual_fuel_oil_cost, :measures => :cost
+          has :monthly_natural_gas_volume_estimate #:measurement_options => { :external => :therms, :internal => :joules }
+          has :monthly_natural_gas_cost, :measures => :cost
+          has :monthly_electricity_use_estimate, :measures => :electrical_energy
+          has :monthly_electricity_cost, :measures => :cost
+          has :green_electricity#, :measures => :percentage
+          has :annual_propane_volume_estimate, :measures => :volume
+          has :annual_propane_cost, :measures => :cost
           has :annual_wood_volume_estimate, :measures => :biomass
-          has :annual_kerosene_volume_estimate, :trumps => :annual_kerosene_cost, :measures => :volume
-          has :annual_coal_volume_estimate, :trumps => :annual_coal_cost, :measures => :mass # it says volume, but it wants mass
-          # has :annual_coal_cost, :trumps => :annual_coal_volume_estimate, :measures => :cost
+          has :annual_kerosene_volume_estimate, :measures => :volume
+          has :annual_coal_volume_estimate, :measures => :mass # it says volume, but it wants mass
+          # has :annual_coal_cost, :measures => :cost
         end
       end
     end
--- lib/residence/data.rb
+++ lib/residence/data.rb
@@ -4,56 +4,54 @@ module BrighterPlanet
   module Residence
     module Data
       def self.included(base)
+        base.create_table do
+          string   'residence_class_id'
+          string   'urbanity_id'
+          string   'zip_code_id'
+          date     'construction_year'
+          boolean  'ownership'
+          float    'occupation'
+          integer  'residents'
+          integer  'floors'
+          float    'floorspace_estimate'
+          string   'air_conditioner_use_id'
+          string   'dishwasher_use_id'
+          string   'clothes_machine_use_id'
+          integer  'refrigerator_count'
+          integer  'freezer_count'
+          float    'lighting_efficiency'
+          integer  'bedrooms'
+          integer  'dining_rooms'
+          integer  'living_rooms'
+          integer  'kitchens'
+          integer  'full_bathrooms'
+          integer  'half_bathrooms'
+          float    'bathrooms'
+          integer  'other_rooms'
+          integer  'heated_garage_berths'
+          float    'annual_fuel_oil_volume_estimate'
+          float    'annual_fuel_oil_cost'
+          float    'monthly_natural_gas_volume_estimate'
+          float    'monthly_natural_gas_cost'
+          float    'monthly_electricity_use_estimate'
+          float    'monthly_electricity_cost'
+          float    'green_electricity'
+          float    'annual_propane_volume_estimate'
+          float    'annual_propane_cost'
+          float    'annual_wood_volume_estimate'
+          float    'annual_kerosene_volume_estimate'
+          float    'annual_coal_volume_estimate'
+          float    'annual_coal_cost'
+          date     'acquisition'
+          date     'retirement'
+        end
+
         base.data_miner do
-          schema do
-            string   'residence_class_id'
-            string   'urbanity_id'
-            string   'zip_code_id'
-            date     'construction_year'
-            boolean  'ownership'
-            float    'occupation'
-            integer  'residents'
-            integer  'floors'
-            float    'floorspace_estimate'
-            string   'air_conditioner_use_id'
-            string   'dishwasher_use_id'
-            string   'clothes_machine_use_id'
-            integer  'refrigerator_count'
-            integer  'freezer_count'
-            float    'lighting_efficiency'
-            integer  'bedrooms'
-            integer  'dining_rooms'
-            integer  'living_rooms'
-            integer  'kitchens'
-            integer  'full_bathrooms'
-            integer  'half_bathrooms'
-            float    'bathrooms'
-            integer  'other_rooms'
-            integer  'heated_garage_berths'
-            float    'annual_fuel_oil_volume_estimate'
-            float    'annual_fuel_oil_cost'
-            float    'monthly_natural_gas_volume_estimate'
-            float    'monthly_natural_gas_cost'
-            float    'monthly_electricity_use_estimate'
-            float    'monthly_electricity_cost'
-            float    'green_electricity'
-            float    'annual_propane_volume_estimate'
-            float    'annual_propane_cost'
-            float    'annual_wood_volume_estimate'
-            float    'annual_kerosene_volume_estimate'
-            float    'annual_coal_volume_estimate'
-            float    'annual_coal_cost'
-            date     'acquisition'
-            date     'retirement'
-          end
-          
           process "pull orphans" do
             ResidenceFuelType.run_data_miner!
             ResidenceFuelPrice.run_data_miner!
             ResidentialEnergyConsumptionSurveyResponse.run_data_miner!
           end
-          
-          process :run_data_miner_on_belongs_to_associations
         end
       end
     end
